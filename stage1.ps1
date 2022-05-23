730d29816c327d5f53fd835c2af4451c871b055b
itcentrenz <help@itcentre.nz>
2022-05-23 13:35:01 +1200

    Moved XAML source to IT Centre Github
    Moved XAML source to IT Centre Github

--- stage1.ps1
+++ stage1.ps1
@@ -207,9 +207,9 @@ catch {
   Invoke-WebRequest -Uri $VCLibsURL -OutFile $VCLibs_path -UseBasicParsing
   Get-Item $VCLibs_path | Unblock-File
   
-  #WINGET Relies on XAML https://docs.microsoft.com/en-us/troubleshoot/cpp/c-runtime-packages-desktop-bridge
-  $UI_XAML_URL = "http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/3c580009-888f-4f54-a0d4-76fb69769b2e?P1=1653263718&P2=404&P3=2&P4=i1D9dGMRb%2beLdH8KzVu4KIIlD%2bRqcvlgeqlddEhbBKyCdsXof7re29pJcdzrCkDP%2bYGT4HXUIlnWiWNLIV6qOg%3d%3d"
-  $UI_XAML_path = "$env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.7_7.2203.17001.0_x86__8wekyb3d8bbwe.appx"
+  #WINGET Relies on XAML 
+  $UI_XAML_URL = "https://github.com/itcentrenz/win10debloat/raw/main/Microsoft.UI.Xaml.2.7_7.2203.17001.0_x64__8wekyb3d8bbwe.Appx"
+  $UI_XAML_path = "$env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.7_7.2203.17001.0_x64__8wekyb3d8bbwe.appx"
   Invoke-WebRequest -Uri $UI_XAML_URL -OutFile $UI_XAML_path -UseBasicParsing
   Get-Item $UI_XAML_path | Unblock-File
 
