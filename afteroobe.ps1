1977123dea8b5f99c2e7ff99fc0923329ea87836
itcentrenz <help@itcentre.nz>
2022-06-17 13:22:22 +1200

    Update afteroobe.ps1

--- afteroobe.ps1
+++ afteroobe.ps1
@@ -82,5 +82,5 @@ $appname = "Microsoft Store"
 ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object{$_.Name -eq $appname}).Verbs() | Where-Object{$_.Name.replace('&','') -match 'Unpin from taskbar'} | ForEach-Object{$_.DoIt(); $exec = $true}
 
 #Remove remaining files
-Remove-Item -Path "C:\temp\*" -Force
+Remove-Item -Path "C:\temp\*" -Force -Recurse
 Restart-Computer -Force -Confirm:$false
