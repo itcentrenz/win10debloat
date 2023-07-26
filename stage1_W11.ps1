4558899d0790b61b01907c22757849626f73f6b9
itcentrenz <help@itcentre.nz>
2023-07-27 09:51:53 +1200

    Update stage1_W11.ps1

--- stage1_W11.ps1
+++ stage1_W11.ps1
@@ -379,10 +379,10 @@ REG ADD "HKLM\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advance
 REG ADD "HKLM\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 1 /f
 
 # Turn on My Computer on the Desktop
-REG ADD "HKLM\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f
+REG ADD "HKLM\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 
 # Turn on User's Files on the Desktop
-REG ADD "HKLM\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {59031a47-3f72-44a7-89c5-5595fe6b30ee} /t REG_DWORD /d 0 /f
+REG ADD "HKLM\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" /t REG_DWORD /d 0 /f
 
 REG UNLOAD HKLM\Default
 Read-Host -prompt "Customized Taskbar and Desktop reg values"
